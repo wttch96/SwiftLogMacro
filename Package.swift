5b4eeaf9077237e3a7ac2cd0ae11bfdbf2fc488d
wttch <wttch@wttch.com>
2024-09-01 12:27:23 +0800

    init

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
     name: "SwiftLogMacro",
@@ -20,6 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,15 +30,20 @@ let package = Package(
             name: "SwiftLogMacroMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
 
         // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "SwiftLogMacro", dependencies: ["SwiftLogMacroMacros"]),
+        .target(
+            name: "SwiftLogMacro",
+            dependencies: [
+                "SwiftLogMacroMacros",
+                .product(name: "Logging", package: "swift-log")
+            ]
+        ),
 
         // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "SwiftLogMacroClient", dependencies: ["SwiftLogMacro"]),
-
     ]
 )
